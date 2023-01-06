f83cff2edeeaafbb299716b49e7924bcc06e3fc9
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-06 06:41:49 -0500

    fix: Cleanup logging and return message

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ addEventListener('fetch', event => {
 
 async function handleRequest(request) {
   const url = new URL(request.url)
-  console.log(request.url)
   const signature =
     (url.searchParams && url.searchParams.get('hex_signature')) ||
     (url.pathname && url.pathname.substring(1))
   if (!signature) {
-    return new Response('Invalid argument', { status: 404 })
+    return new Response('Missing signature', { status: 404 })
   }
   const value = await NAMESPACE.get(signature, { type: 'json' })
   if (value === null) {
@@ -22,13 +21,16 @@ async function handleRequest(request) {
       }
       const output = await request.json()
       await NAMESPACE.put(signature, JSON.stringify(output))
+      console.log(`cache miss and populated ${signature}`)
       return new Response(JSON.stringify(output), {
         headers: { 'content-type': 'application/json' },
       })
     } catch (err) {
+        console.log(`cache miss failed ${signature}`)
       return new Response(err.message, { status: 500 })
     }
   }
+  console.log(`cache hit ${signature}`)
   return new Response(JSON.stringify(value), {
     headers: { 'content-type': 'application/json' },
   })
