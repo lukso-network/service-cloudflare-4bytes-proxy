e59aa0ccec1cbeba192b846c9d5cee86c1bb3eb8
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-06 06:47:33 -0500

    fix: Add cors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+const cors = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+}
+
 async function handleRequest(request) {
   const url = new URL(request.url)
   const signature =
     (url.searchParams && url.searchParams.get('hex_signature')) ||
     (url.pathname && url.pathname.substring(1))
   if (!signature) {
-    return new Response('Missing signature', { status: 404 })
+    return new Response('Missing signature', { status: 404, headers: cors })
   }
   const value = await NAMESPACE.get(signature, { type: 'json' })
   if (value === null) {
@@ -17,21 +23,21 @@ async function handleRequest(request) {
         `https://www.4byte.directory/api/v1/signatures/?hex_signature=${signature}`
       )
       if (request.status != 200) {
-        return new Response(request.statusText(), { status: request.status })
+        return new Response(request.statusText(), { status: request.status, headers: cors })
       }
       const output = await request.json()
       await NAMESPACE.put(signature, JSON.stringify(output))
       console.log(`cache miss and populated ${signature}`)
       return new Response(JSON.stringify(output), {
-        headers: { 'content-type': 'application/json' },
+        headers: { 'content-type': 'application/json', ...cors },
       })
     } catch (err) {
         console.log(`cache miss failed ${signature}`)
-      return new Response(err.message, { status: 500 })
+      return new Response(err.message, { status: 500, headers: cors })
     }
   }
   console.log(`cache hit ${signature}`)
   return new Response(JSON.stringify(value), {
-    headers: { 'content-type': 'application/json' },
+    headers: { 'content-type': 'application/json', ...cors },
   })
 }
