c703064234c06f599e960c45ea86a5eece737bf1
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-01-26 09:03:44 -0500

    fix: Hex filter

--- src/index.js
+++ src/index.js
@@ -1,51 +1,54 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
 
 const cors = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS',
-  'Access-Control-Max-Age': '86400',
-  'Access-Control-Allow-Headers': 'content-type'
-}
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+  "Access-Control-Allow-Headers": "content-type",
+};
 
 async function handleRequest(request) {
-  const url = new URL(request.url)
+  const url = new URL(request.url);
   const signature =
-    (url.searchParams && url.searchParams.get('hex_signature')) ||
-    (url.pathname && url.pathname.substring(1))
+    (url.searchParams && url.searchParams.get("hex_signature")) ||
+    (url.pathname && url.pathname.substring(1));
   if (!signature) {
-    return new Response('Missing signature', { status: 404, headers: cors })
+    return new Response("Missing signature", { status: 404, headers: cors });
   }
-  if (!/(0x)?[0-9]{8}/.test(signature)) {
-    return new Response('Invalid request', { status: 500, headers: cors })
+  if (!/(0x)?[0-9a-f]{8}/.test(signature)) {
+    return new Response("Invalid request", { status: 500, headers: cors });
   }
-  const value = await NAMESPACE.get(signature, { type: 'json' })
+  const value = await NAMESPACE.get(signature, { type: "json" });
   if (value === null) {
     try {
       const request = await fetch(
         `https://www.4byte.directory/api/v1/signatures/?hex_signature=${signature}`
-      )
+      );
       if (request.status != 200) {
-        return new Response(request.statusText(), { status: request.status, headers: cors })
+        return new Response(request.statusText(), {
+          status: request.status,
+          headers: cors,
+        });
       }
-      const output = await request.json()
+      const output = await request.json();
       if (output.count !== 0) {
-        await NAMESPACE.put(signature, JSON.stringify(output))
-        console.log(`cache miss and populated ${signature}`)
+        await NAMESPACE.put(signature, JSON.stringify(output));
+        console.log(`cache miss and populated ${signature}`);
       } else {
-        console.log(`cache miss and ignored (count == 0) ${signature}`)
+        console.log(`cache miss and ignored (count == 0) ${signature}`);
       }
       return new Response(JSON.stringify(output), {
-        headers: { 'content-type': 'application/json', ...cors },
-      })
+        headers: { "content-type": "application/json", ...cors },
+      });
     } catch (err) {
-        console.log(`cache miss failed ${signature}`)
-      return new Response(err.message, { status: 500, headers: cors })
+      console.log(`cache miss failed ${signature}`);
+      return new Response(err.message, { status: 500, headers: cors });
     }
   }
-  console.log(`cache hit ${signature}`)
+  console.log(`cache hit ${signature}`);
   return new Response(JSON.stringify(value), {
-    headers: { 'content-type': 'application/json', ...cors },
-  })
+    headers: { "content-type": "application/json", ...cors },
+  });
 }
